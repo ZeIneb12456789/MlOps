791cea68bc06b4232e57cc375fc49fea1679bdef
ZeIneb12456789 <zeineb.bouattour@esprit.tn>
2025-02-25 18:25:21 +0000

    added safety for app

--- model_service/app.py
+++ model_service/app.py
@@ -61,6 +61,8 @@ def predict():
     except Exception as e:
         abort(500, str(e))
 
+debug_mode = os.getenv("FLASK_DEBUG", "False").lower() == "true"
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=debug_mode, host="127.0.0.1", port=5001)
 
